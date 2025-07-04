171a6180c39e4b6f83978436a7579ccdc16ed359
roshan-jameer <skroshanjameer325@gmail.com>
2025-07-04 23:54:23 +0530

    commit before payment api

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import {isAuth,isAdmin} from "../middlewares/isAuth.js";
-import { addLectures, createCourse } from '../controllers/admin.js';
-import { uploadFiles } from "../middlewares/multer.js"
+import { addLectures,
+         createCourse ,
+         deleteLecture,
+         getAllStats
+        } from '../controllers/admin.js';
+import { uploadFiles } from "../middlewares/multer.js";
+
 
 const router=express.Router();
 router.post('/course/new',isAuth,isAdmin,uploadFiles,createCourse);
 router.post('/course/:id',isAuth,isAdmin,uploadFiles,addLectures);
+router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
+router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
+router.get("/stats", isAuth, isAdmin, getAllStats);
 
 
 export default router;
